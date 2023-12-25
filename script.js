147d962418a32b713d502d339f5e321d313def3c
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-25 19:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,16 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0;
+  for (let book of library) {
+    if (book.readingStatus) {
+      count++;
+    }
+  }
+  return count;
+	
 };
 
 // Do not change the code below
 
-alert(numberOfBooksRead());
+// alert(numberOfBooksRead());
